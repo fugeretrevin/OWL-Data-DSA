0262c4de16c824eae439bee8fcbef48dc6deb6c9
fugeretrevin <fugeretrevin@gmail.com>
2025-10-30 14:28:51 -0400

    added header container

--- data.py
+++ data.py
@@ -4,5 +4,5 @@ df = pd.read_csv('./OWL-data/phs_2018_playoffs.csv', encoding='latin1')
 print(df.head())
 print(df['stat_amount'].mean())
 print(df.loc[(df['stat_name'] == 'All Damage Done') & (df['hero'] != 'All Heroes'), 'stat_amount'].mean())
-newdf = df[(df['stat_name'] == 'All Damage Done') & (df['hero'] != 'All Heroes') ]
-print(newdf.head())
+damage_df = df[(df['stat_name'] == 'All Damage Done') & (df['hero'] != 'All Heroes') ]
+print(damage_df.head())
