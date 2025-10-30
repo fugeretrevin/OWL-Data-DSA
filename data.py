8e80eb67b09c2fe69f99cef7aa14bbdc70f6ccfd
fugeretrevin <fugeretrevin@gmail.com>
2025-10-30 13:16:31 -0400

    added dataframe to manipulate data - can be used to filter data

--- data.py
+++ data.py
@@ -1,4 +1,8 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv('./OWL-data/phs_2018_playoffs.csv')
+df = pd.read_csv('./OWL-data/phs_2018_playoffs.csv', encoding='latin1')
 print(df.head())
+print(df['stat_amount'].mean())
+print(df.loc[(df['stat_name'] == 'All Damage Done') & (df['hero'] != 'All Heroes'), 'stat_amount'].mean())
+newdf = df[(df['stat_name'] == 'All Damage Done') & (df['hero'] != 'All Heroes') ]
+print(newdf.head())
